351765bea25745b30b9a26411521f82e46e68608
Felipe Abello <pipe@helloguru.io>
2023-02-22 22:30:00 -0800

    update sms copies

--- sms_handler.py
+++ sms_handler.py
@@ -5,11 +5,11 @@ import os
 account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
 auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 
-welcome_message = "Hello :) I'm Nara, your AI nutritional assistant.\n\nImagine you have a full-time healthy friend. This is a very smart friend that knows your dietary preferences and has delicioius recipe suggestions.\n\nBefore we start, I need to understand your preferences here: https://airtable.com/shrwrO2q0cepBaffV?hide_phone=true&prefill_phone="
+welcome_message = "👋 Hello! I'm Nara, your AI nutritional assistant.\n🍎🥦 Imagine having a full-time healthy friend - someone smart who knows your dietary preferences and has delicious recipe suggestions for you.\n👉 But before we get started, I need to understand your preferences. Please take a moment to fill out this short survey: [link] https://airtable.com/shrwrO2q0cepBaffV?hide_phone=true&prefill_phone="
 
-thanks_for_uploading_message = "Thanks for uploading your PDF. I am training myself using the provided PDF. Please hand tight, I'll let you know as soon as I'm ready in a couple of minutes."
+thanks_for_uploading_message = "👋 Thanks for completing the survey! We're 🔧 setting up your account and will notify you by 📱 SMS when it's ready. In the meantime, feel free to 💭 think about any questions you have. I'm excited to 🤝 work with you to help you achieve your health goals!"
 
-trained_finished_message = "I've finished training on the provided PDF. You can now take a picture or upload a screenshot of a menu and I'll recommend what you can order."
+trained_finished_message = "🎉 Your Nara account is now ready. As your personal AI nutritional assistant, I'm excited to help you achieve your health goals!\n🍽️ If you're not sure where to start, feel free to ask me for recipe suggestions. For example, you could ask, \"What can I eat for lunch today?\" and I'll provide you with a list of delicious and healthy options based on your dietary preferences.\n💬 I'm here to support you every step of the way, so don't hesitate to ask me any questions or share your concerns. Let's work together to make healthy eating easy and enjoyable!"
 
 def send_sms(to_phone_number, step, response_message=""):
 
